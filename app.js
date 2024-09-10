00dbebcd54b66d11390fc6b3fb78d9bcddad0fc8
maimoona <maimoonaaltaf7@gmail.com>
2024-09-10 01:19:47 -0700

    2nd asignment

---
+++ app.js
@@ -0,0 +1,247 @@
+// chap-6    ( MATH EXPRESSION )
+
+
+// Q-1
+// ADD
+
+// var num1 = 2;
+// var num2 = 4;
+// var res = num1 + num2;
+// document.write ( "Sum of 2 and 4 =" +res+ "<br>")
+
+
+// Q-2
+// Sub
+
+// var num1 = 8;
+// var num2 = 3;
+// var res = num1 - num2;
+// document.write ( "sum of 8 and 3 =" +res+ "<br>" )
+
+
+// Multiplication
+
+// var num1 = 7;
+// var num2 = 5;
+// var res = num1 * num2;
+// document.write ( "sum of 7 and 5 =" +res+ "<br>" )
+
+
+// Divide
+
+// var num1 = 8;
+// var num2 = 3;
+// var res = num1 / num2;
+// document.write ( "sum of 4 and 3 =" +res+ "<br>" );
+
+
+
+// Q-3
+
+// var variable = 12;
+// document.write ( "The value of variable is : " + variable + "<br>" );
+
+
+// var variable = 5;
+// document.write ( "Initial value is : " + variable + "<br>" );
+
+
+// var value = 8;
+// document.write ( "Value after increment is : " + value + "<br>" )
+
+
+// var add = 13;
+// document.write ( "Value of after addition is : " + add + "<br>" )
+
+
+// var add = 10;
+// document.write ( "Value of after decrement is : " + add + "<br>" )
+
+
+// var reminder = 0;
+// document.write ( "The reminder is : " + reminder + "<br>" )
+
+
+
+// Q-4
+// var movieTicket = "Total cost to buy 4 Tickets to a movie is 5,000 PKR";
+// document.write ( movieTicket + "<br>" );
+
+
+
+// Q-5
+
+
+
+
+// Q-6
+// -------
+
+
+// Q-7
+// var item1 = "Price of item 1 is 650" + "<br>";
+// document.write ( item1 );
+
+// var Quantity = "Quantity of item 1 is 3" + "<br>" ;
+// document.write ( Quantity );
+
+// var item2 = "Price of item 2 is 100" + "<br>" ;
+// document.write ( item2 );
+
+// var Quantity2 = "Quantity of item 2 is 7" + "<br>" ;
+// document.write ( Quantity2 );
+
+// var shipping = "Shipping charges 100" + "<br>" + "<br>"  ;
+// document.write ( shipping );
+
+
+// var Total = "Total cost of your order is 2750" + "<br>" ;
+// document.write ( Total );
+
+
+// Q-8
+// var Marks = "Total Marks : " + 980 + "<br>"   ;
+// document.write ( Marks );
+
+// var  obtained = " Marks Obtained : " + 804 + "<br>"   ;
+// document.write ( obtained );
+
+// var  percentage = " Percentage : " + 84.43 + "<br>"   ;
+// document.write ( percentage );
+
+
+// Q-9
+
+// var num1 = 104.80;
+// var num2 = 28;
+// var res = num1 + num2;
+// document.write ( "Total currency in PKR = " +res+ "<br>" )
+
+
+
+// Q-10
+
+// var add = 5 + 5;
+// document.write ( add  + "<br>"  );
+
+
+// var Multiplication = 10 * 10;
+// document.write ( Multiplication + "<br>" );
+
+
+// var Divide = 100 / 2;
+// document.write ( Divide );
+
+
+
+// Q-11
+// var year = "Current year :" + 2024 ;
+// document.write ( year + "<br>" );
+
+
+// var birthyear = "Birth year :" + 2009 ;
+// document.write ( birthyear + "<br>" );
+
+
+// var age = " your age is:" + 15 ;
+// document.write ( age );
+
+
+
+// Q-12
+ 
+// var circle = "Radius of circle is : " + 20;
+// document.write ( circle + "<br>" );
+
+
+// var circus = "The Circumference is : " + 125.6667499999 ;
+// document.write ( circus + "<br>");
+
+
+// var area = "The area is : " + 1256.89;
+// document.write ( area );
+
+
+
+
+// Q-13
+//  var favouriteSnack = "Favourite snack : Lays wavy";
+//  document.write ( favouriteSnack + "<br>" );
+
+//  var Age = "Current age : " + 15;
+//  document.write ( Age + "<br>" );
+
+//  var Age = "Estimated maximum age : " + 20;
+//  document.write ( Age + "<br>" );
+
+//  var snacks = "Amount of snacks per Day : " + 2;
+//  document.write ( snacks + "<br>" );
+
+
+
+
+
+//  Chap-6    ( Math Expression )
+
+
+
+// Q-1
+var abc = ( "Result" );
+document.write ( abc + "<br>"  );
+
+var value = ("The value of a is : " + 10) 
+document.write ( value + "<br>" + "<br>" )
+
+
+// ----
+var value = ("The value of ++a is : " + 11) 
+document.write ( value + "<br>" )
+
+var value = ("Now value of a is : " + 11) 
+document.write ( value + "<br>" + "<br>" )
+
+
+// ----
+var value = ("The value of a++ is : " + 11) 
+document.write ( value + "<br>" )
+
+var value = ("Now value of a is : " + 12) 
+document.write ( value + "<br>" +"<br>" )
+
+
+// ----
+var value = ("The value of --a is : " + 11) 
+document.write ( value + "<br>" )
+
+var value = ("Now value of a is : " + 11) 
+document.write ( value + "<br>" + "<br>")
+
+
+// ----
+var value = ("The value of a-- is : " + 11) 
+document.write ( value + "<br>" )
+
+var value = ("Now value of a is : " + 10) 
+document.write ( value + "<br>" )
+
+
+
+// Q-2
+
+// ---
+
+
+// Q-3
+
+prompt ( "Enter your name" );
+
+
+// Q-5
+
+
+
+// Q-6
+
+
+
+// Q-7
